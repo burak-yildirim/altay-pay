6c02c3b51df06fb32b724cc6e771d74362e68d2c
burak-yildirim <yildirim.burak@protonmail.com>
2019-11-13 02:34:54 +0300

    tank panel and pos panel can communicate through socket now

--- utils/socket.js
+++ utils/socket.js
@@ -1,19 +1,21 @@
 function ioBuilder(server) {
   const io = require('socket.io')(server);
   function posSendHandler(data) {
+    console.log('posSendHandler triggered');
     io.emit('fromPos', data);
   }
   function tankSendHandler(data) {
+    console.log('tankSendHandler triggered');
     io.emit('fromTank', data);
   }
 
-  io.on('connection', function (client) {
-    console.log(`${client.id} has joined to the socket server`);
+  io.on('connection', function (socket) {
+    console.log(`${socket.id} has joined to the socket server`);
 
-    io.on('toCustomer', posSendHandler);
-    io.on('toPos', tankSendHandler);
+    socket.on('toTank', posSendHandler);
+    socket.on('toPos', tankSendHandler);
+    socket.on('disconnect', (socket) => console.log(`a socket disconnected`));
   });
-  io.on('close', (client) => console.log(`${client.id} has left`))
 
   return io;
 }
